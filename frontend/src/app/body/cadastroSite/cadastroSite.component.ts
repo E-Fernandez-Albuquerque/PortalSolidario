77c3a2ad9c4e26589d297adc7af8f43d8b34ace1
edsay12 <edvandearaujo2@hotmail.com>
2021-11-04 09:04:20 -0300

    tela de cadastro 1

---
+++ frontend/src/app/body/cadastroSite/cadastroSite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-CadastroSite',
+  templateUrl: './cadastroSite.component.html',
+  styleUrls: ['./cadastroSite.component.css']
+})
+export class CadastroSiteComponente implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+}
